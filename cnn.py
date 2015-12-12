ea4a7868b6bf5eec8a908e819ed5536be12491c4
Julian Kniephoff <me@juliankniephoff.com>
2015-12-12 14:29:43 +0100

    Fix extracting vocabulary

--- cnn.py
+++ cnn.py
@@ -7,7 +7,7 @@ with open(tweets_path) as tweets_file:
     for row in csv.reader(tweets_file):
         tweet = row[1:]
         tweets.append(tweet)
-        for word in tweets:
+        for word in tweet:
             vocabulary.add(word)
 
     print(len(tweets), 'Tweets')
