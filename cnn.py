54022f07e72b75e539449654fbd665950a3a429c
Julian Kniephoff <me@juliankniephoff.com>
2015-12-14 15:45:07 +0100

    Try converting tweets to index sequences

--- cnn.py
+++ cnn.py
@@ -7,17 +7,25 @@ import numpy as np
 from gensim.models import Word2Vec
 
 
+class CNN:
+    def __init__(self, initial_model=None, vocab=None):
+        self.initial_model = initial_model
+        self.vocab = vocab
+
+        if not vocab:
+            assert initial_model
+
+            self.vocab = list(self.initial_model.vocab)
+
+        self.index = {word: i for (i, word) in enumerate(self.vocab)}
+
+    def tweet_to_indices(self, tweet):
+        return [self.index[word] for word in tweet if word in self.vocab]
+
+
 def parse_tweets(path):
     with open(path) as tweets_file:
-        tweets = list()
-        vocabulary = set()
-        for row in csv.reader(tweets_file):
-            tweet = row[1:]
-            tweets.append(tweet)
-            for word in tweet:
-                vocabulary.add(word)
-
-        return tweets, vocabulary
+        return [row[1:] for row in csv.reader(tweets_file)]
 
 
 def main():
@@ -30,17 +38,26 @@ def main():
         sys.exit(1)
 
     # Load tweets and vocabulary
-    positive_tweets, positive_vocabulary = parse_tweets(positive_tweets_path)
-    negative_tweets, negative_vocabulary = parse_tweets(negative_tweets_path)
+    positive_tweets = parse_tweets(positive_tweets_path)
+    negative_tweets = parse_tweets(negative_tweets_path)
 
-    tweets = positive_tweets + negative_tweets
-    # The vocabulary has to become a list since we need to keep track of the order
-    vocabulary = list(positive_vocabulary | negative_vocabulary)
+    cnn = CNN(Word2Vec.load(embeddings_path))
+    print(cnn.tweet_to_indices(positive_tweets[0]))
+    print(cnn.tweet_to_indices(['laksdfjalskdfjalsdfkjasdlkfjasldfkjasdlfkj',
+                                'laksdfjlasdjflkasdjfasldkfjadslkfjaslkdfjasdlkfjaslkdfjadslkjfasldkfj']))
 
     # Extract initial weights for the embedding layer
-    word2vec_embeddings = Word2Vec.load(embeddings_path)
-    weights = np.array(map(lambda word: word2vec_embeddings[word], vocabulary))
-    print(weights)
+    # word2vec_embeddings = Word2Vec.load(embeddings_path)
+    # weights = list()
+    # for word in vocabulary:
+    #    try:
+    #        weights.append(word2vec_embeddings[word])
+    #    except KeyError:
+    #        pass
+
+    ## TODO There might be stuff in here that we do not have embeddings for
+    # weights = np.array(map(lambda word: word2vec_embeddings[word], vocabulary))
+    # print(weights)
 
 
 if __name__ == '__main__':
